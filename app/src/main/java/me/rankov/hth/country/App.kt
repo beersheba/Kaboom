68f6ef45d175e7d3c90b1bf058b98ea1e226f2c4
Andrey Rankov <andreyr@appcard.com>
2018-11-13 10:02:09 +0200

    Instabug integration

---
+++ app/src/main/java/me/rankov/hth/country/App.kt
@@ -0,0 +1,16 @@
+package me.rankov.hth.country
+
+import android.app.Application
+import com.instabug.library.Instabug
+import com.instabug.library.invocation.InstabugInvocationEvent
+
+
+
+class App: Application() {
+    override fun onCreate() {
+        super.onCreate()
+        Instabug.Builder(this, "ed4eb5fa7787022e45d209759c1e2381")
+                .setInvocationEvents(InstabugInvocationEvent.SHAKE, InstabugInvocationEvent.SCREENSHOT)
+                .build()
+    }
+}
