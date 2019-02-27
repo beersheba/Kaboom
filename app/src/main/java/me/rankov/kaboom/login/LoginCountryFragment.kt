04557ba54cd1f427431eee52ef2654820e0916e7
Andrey Rankov <andreyr@appcard.com>
2019-02-27 10:36:21 +0200

    Fragment animation added

--- app/src/main/java/me/rankov/kaboom/login/LoginCountryFragment.kt
+++ app/src/main/java/me/rankov/kaboom/login/LoginCountryFragment.kt
@@ -7,6 +7,7 @@ import android.view.View
 import android.view.ViewGroup
 import android.widget.Button
 import androidx.navigation.fragment.findNavController
+import androidx.navigation.navOptions
 import me.rankov.kaboom.R
 
 class LoginCountryFragment : Fragment() {
@@ -16,11 +17,20 @@ class LoginCountryFragment : Fragment() {
         return inflater.inflate(R.layout.fragment_login_country, container, false)
     }
 
+    private val options = navOptions {
+        anim {
+            enter = R.anim.slide_in_right
+            exit = R.anim.slide_out_left
+            popEnter = R.anim.slide_in_left
+            popExit = R.anim.slide_out_right
+        }
+    }
+
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
         val button = view.findViewById<Button>(R.id.country_next_button)
         button?.setOnClickListener {
-            findNavController().navigate(R.id.loginNameFragment)
+            findNavController().navigate(R.id.loginNameFragment, null, options)
         }
     }
 }
