a0657882cd965e295dd51258b4653cf9ab4c5040
Andrey Rankov <andreyr@appcard.com>
2019-04-15 11:23:42 +0300

    Instabug remove

--- app/src/main/java/me/rankov/kaboom/App.kt
+++ app/src/main/java/me/rankov/kaboom/App.kt
@@ -2,8 +2,6 @@ package me.rankov.kaboom
 
 import android.app.Application
 import android.content.Context
-import com.instabug.library.Instabug
-import com.instabug.library.invocation.InstabugInvocationEvent
 
 val prefs: Prefs by lazy {
     App.prefs!!
@@ -27,8 +25,5 @@ class App : Application() {
     override fun onCreate() {
         prefs = Prefs(applicationContext())
         super.onCreate()
-        Instabug.Builder(this, "ed4eb5fa7787022e45d209759c1e2381")
-                .setInvocationEvents(InstabugInvocationEvent.SHAKE, InstabugInvocationEvent.SCREENSHOT)
-                .build()
     }
 }
